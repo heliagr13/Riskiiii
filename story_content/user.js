0703775670562ecc2e74133daf9c795cf7e18e6b
heliagr13 <ol.helgos@gmail.com>
2026-09-03 16:38:29 +0300

    1st

---
+++ story_content/user.js
@@ -0,0 +1,61 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LWRdEBUN5C":
+        Script1();
+        break;
+      case "5qNF0VUigU1":
+        Script2();
+        break;
+      case "6Vd3f1Vo2nR":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer(); 
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+//set score; the first number is the score 
+SCORM2004_SetScore(100, 100, 0);
+}
+
+function Script2()
+{
+  var player = GetPlayer(); 
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+//set score; the first number is the score 
+SCORM2004_SetScore(100, 100, 0);
+}
+
+function Script3()
+{
+  var player = GetPlayer(); 
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+var lmsAPI = findLMSAPI(this);
+
+//set score; the first number is the score 
+SCORM2004_SetScore(100, 100, 0);
+}
+
